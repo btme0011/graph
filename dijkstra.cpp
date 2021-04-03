1966af4bef2ae38fec0334b2991eabaaa9f5dacc
btme0011 <71667635+btme0011@users.noreply.github.com>
2021-04-04 02:01:30 +0530

    Update dijkstra.cpp

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -1,13 +1,30 @@
-
-while(!pq.empty()){
-	  curr=pq.top();
-		visited.insert(curr);
-  	pq.pop();
-
-		for(auto x:edge[curr]){// things which are connected by that node
-				pq.push(x);
+	vector<vector<pair<ll,ll>> edge(n);
+	for(int i=0;i<m;i++) {
+		ll a,b,c;
+		cin>>a>>b>>c;
+		a--;
+		b--;
+		edge[a].push_back({c,b});
+		edge[b].push_back({c,a});
+	}
+	
+	
+	priority_queue<tuple<ll,ll,ll>,vector<tuple<ll,ll,ll>>,greater<tuple<ll,ll,ll>>> pq;
+	pq.push({0,0});
+	set<ll> visited;
+	//visited.insert(0);
+	ll cnt=0;
+	vector<ll> ans;
+	while(!pq.empty()){
+		pair<ll,ll> curr=pq.top();
+		if(visited.count(curr.second)!=0) {pq.pop();continue;}
+		visited.insert(curr.second);
+		pq.pop();		
+		
+		
+		for(auto x:edge[curr.second]){// things which are connected by that node
+			if(visited.count(x.second)==0)	{
+				pq.push({x.first+curr.first,x.second});
+			}
 		}
 	}
-
-
-// multiple source djastra
