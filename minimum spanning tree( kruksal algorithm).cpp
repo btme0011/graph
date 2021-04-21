2820025ebc41dfdc7ab0d9ae671c615a1f6bf74f
btme0011 <71667635+btme0011@users.noreply.github.com>
2021-04-21 11:37:50 +0530

    Update minimum spanning tree( kruksal algorithm).cpp

--- minimum spanning tree( kruksal algorithm).cpp
+++ minimum spanning tree( kruksal algorithm).cpp
@@ -1,45 +1,53 @@
-string solve(){
-	int n,m;
-	cin>>n>>m;
+template<typename T>
+struct mst{
+	
+	mst(T n,vector<tuple<T,T,T>> edge){// no of nodes,<weight,node a, node b>
+		solve(n,edge);
+	}
+	
+	
+	string solve(T n,vector<tuple<T,T,T>> edge){
+// 		int n,m;
+// 		cin>>n>>m;
 
-	vector<tuple<ll,ll,ll>> edge;
-	for(int i=0;i<m;i++){
-		ll a,b,c;
-		cin>>a>>b>>c;
+// 		vector<tuple<ll,ll,ll>> edge;
+// 		for(int i=0;i<m;i++){
+// 			ll a,b,c;
+// 			cin>>a>>b>>c;
+
+// 			edge.push_back({c,a,b});
+// 		}
+		vector<tuple<ll,ll,ll>> ans;
+		sort(edge.begin(),edge.end());
+
+		vector<T> par(n);
+		vector<T> size(n);
+
+		for(int i=0;i<n;i++){
+			par[i]=i;
+			size[i]=1;
+		}
+
 		
-		edge.push_back({c,a,b});
-	}
-	
-	sort(edge.begin(),edge.end());
-	
-	vector<ll> par(n);
-	vector<ll> size(n);
-	
-	for(int i=0;i<n;i++){
-		par[i]=i;
-		size[i]=1;
-	}
-	
-	vector<tuple<ll,ll,ll>> ans;
-	for(int i=0;i<(ll)edge.size();i++){
-		ll a=get<1>(edge[i]);
-		ll b=get<2>(edge[i]);
-		while(a!=par[a]){
-			a=par[a];
-		}
-		while(b!=par[b]){
-			b=par[b];
-		}
-		if(a!=b){
-			ans.push_back({a,b,get<0>(edge[i])});
-			if(size[a]>size[b]){
-				par[b]=par[a];
-				size[a]+=size[b];
-			}else{
-				par[a]=par[b];
-				size[b]+=size[a];
+		for(int i=0;i<(ll)edge.size();i++){
+			ll a=get<1>(edge[i]);
+			ll b=get<2>(edge[i]);
+			while(a!=par[a]){
+				a=par[a];
+			}
+			while(b!=par[b]){
+				b=par[b];
+			}
+			if(a!=b){
+				ans.push_back({a,b,get<0>(edge[i])});
+				if(size[a]>size[b]){
+					par[b]=par[a];
+					size[a]+=size[b];
+				}else{
+					par[a]=par[b];
+					size[b]+=size[a];
+				}
 			}
 		}
 	}
-       for(auto x:ans) {deb(get<0>(x),get<1>(x),get<2>(x));}
-}
+};
