ab93d693ca0773ec3a7b987f6c1400c23a16f225
btme0011 <71667635+btme0011@users.noreply.github.com>
2021-04-21 09:19:56 +0530

    Update bfs.cpp

--- bfs.cpp
+++ bfs.cpp
@@ -1,29 +1,45 @@
-queue<ll> q;
-q.push(0);// initial element in queue
-q.push(INF);
-set<ll> vis;
-vis.insert(0);
-while(!q.empty()){
-		ll curr=q.front();
-		q.pop();
-		
-		for(ll x:edge[curr]){
-			if(vis.count(x)==0){
-				q.push(x);
-				vis.insert(x);
-			}
-		}
-		
-		
-		// do what u want to do with curr element
-    
-		
-		if(q.front()==INF){
-			if(q.size()==1) break;
-			q.pop();
-			sign=!sign;
-			q.push(INF);
-		}
-		
+template<typename T>
+struct bfs{
+	queue<T> q;
+	
+	q.push(INF);
+	vector<bool> vis;
+	
+	
+	T n;
+	vector<vector<T>> edge;
+	bfs(T s,vector<vector<T>> e){
+		edge=e;
+		n=s;
+		vis=vector<bool>(s);
+		start();
 	}
+	
+	void start(){
+		q.push(0);// initial element in queue
+		vis.insert(0);
+		while(!q.empty()){
+			T curr=q.front();
+			q.pop();
+
+			for(ll x:edge[curr]){
+				if(vis.count(x)==0){
+					q.push(x);
+					vis[x]=1;
+				}
+			}
+
+
+			// do what u want to do with curr element
+
+
+			if(q.front()==INF){
+				if(q.size()==1) break;
+				q.pop();
+				q.push(INF);
+			}
+
+		}
+	}
+};
 	
