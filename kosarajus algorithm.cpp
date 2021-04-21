a63e26b3b5e5572f2263cdf40bd2a9fb1f62f69e
btme0011 <71667635+btme0011@users.noreply.github.com>
2021-04-21 11:24:22 +0530

    Update kosarajus algorithm.cpp

--- kosarajus algorithm.cpp
+++ kosarajus algorithm.cpp
@@ -1,50 +1,45 @@
- stack<ll> st;
-set<ll> visited;
-void dfs1(ll node,vector<vector<ll>> edge){
-	visited.insert(node);
-	for(ll x:edge[node]){
-		if(visited.count(x)==0) dfs1(x,edge);
+template<typename T>
+struct scc{
+	vector<bool> vis;
+	vector<vector<T>> last;// final ans all the strongly connected component
+	scc(T n,vector<vector<T>> edge1,vector<vector<T>> edge2){// no of nodes , edge , reverse of edge
+		vis=vector<bool>(n+1);
+		solve(n,edge1,edge2);
 	}
-	deb(node);
-	st.push(node);
-}
-
-vector<ll> ans;
-void dfs2(ll node,vector<vector<ll>> edge){
-	//deb(node);
-	visited.insert(node);
-	ans.push_back(node);
-	for(ll x:edge[node]){
-		if(visited.count(x)==0) dfs2(x,edge);
-	}
-}
-
-void solve(){
-	ll n,m;
-	cin>>n>>m;
 	
-	vector<vector<ll>> edge1(n);
-	vector<vector<ll>> edge2(n);
-	fo(i,m){
-		int a,b;
-		cin>>a>>b;
+	stack<ll> st;
+	void dfs1(T node,vector<vector<ll>> edge){
+		vis[node]=1;
+		for(ll x:edge[node]){
+			if(vis[x]==0) dfs1(x,edge);
+		}
+		st.push(node);
+	}
+
+	vector<ll> ans;
+	void dfs2(T node,vector<vector<T>> edge){
+		//deb(node);
+		vis[node]=1;
+		ans.push_back(node);
+		for(ll x:edge[node]){
+			if(vis[x]==0) dfs2(x,edge);
+		}
+	}
+
+	void solve(T n,vector<vector<T>> edge1,vector<vector<T>> edge2){
+
+
+		for(int i=0;i<n;i++)
+			if(visited.count(i)==0)
+				dfs1(i,edge1);
+
+		for(auto &x:vis) x=0;
 		
-		edge1[a].push_back(b);
-		edge2[b].push_back(a);
+		while(!st.empty()){
+			while(vis[st.top()]!=0) st.pop();
+			dfs2(st.top(),edge2);
+			last.push_back(ans);
+			ans.clear();
+		}
 	}
-	
-	
-	for(int i=0;i<n;i++)
-		if(visited.count(i)==0)
-			dfs1(i,edge1);
-	
-	visited.clear();
-	
-	while(!st.empty()){
-		while(visited.count(st.top())!=0) st.pop();
-		dfs2(st.top(),edge2);
-		vdeb(ans);
-		ans.clear();
-	}
-	
 }
