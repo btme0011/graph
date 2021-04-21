a2fe972002255f1f8d843632433bbee0e1d9d583
btme0011 <71667635+btme0011@users.noreply.github.com>
2021-04-21 10:08:37 +0530

    Update bridges.cpp

--- bridges.cpp
+++ bridges.cpp
@@ -1,15 +1,33 @@
-void dfs(int v, int p = -1) {
-    visited[v] = true;
-    tin[v] = low[v] = timer++;
-    for (int to : adj[v]) {
-        if (to == p) continue;
-        if (visited[to]) {
-            low[v] = min(low[v], tin[to]);
-        } else {
-            dfs(to, v);
-            low[v] = min(low[v], low[to]);
-            if (low[to] > tin[v])
-                IS_BRIDGE(v, to);
+template<typename T>
+struct bridge{
+    vector<vector<T>> edge;
+    vector<bool> vis;
+    vector<T> tin;
+    vector<T> tout;
+    T timer=0;
+    T n;
+    vector<pair<T,T>> ans;
+    bridge(T node,vector<vector<T>> e,T start){// no of nodes , edge , startting node
+        edge=e;
+        n=node;
+        vis=vector<bool>(n);
+        tin=vector<T>(n);
+        tout=vector<T>(n);
+        dfs(start,-1);
+    }
+    void dfs(int v, int p) {
+        visited[v] = true;
+        tin[v] = low[v] = timer++;
+        for (int to : adj[v]) {
+            if (to == p) continue;
+            if (visited[to]) {
+                low[v] = min(low[v], tin[to]);
+            } else {
+                dfs(to, v);
+                low[v] = min(low[v], low[to]);
+                if (low[to] > tin[v])
+                    ans.push_back({v, to});
+            }
         }
     }
-}
+};
