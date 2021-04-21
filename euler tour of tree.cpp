d1c6bc7bfce30d294ba40b0ee1445088ee882e2b
btme0011 <71667635+btme0011@users.noreply.github.com>
2021-04-21 10:52:29 +0530

    Update euler tour of tree.cpp

--- euler tour of tree.cpp
+++ euler tour of tree.cpp
@@ -1,18 +1,29 @@
-int in[N];
-int euler[2*N];
-int out[N];
-int time=0;
-
-void dfs(int node,vector<vector<int>> edge,int par){
+template<typename T>
+struct ett{
+	vector<T> in;
+	vector<T> euler;// ans
+	vector<T> out;
+	int time=0;
 	
-	in[node]=timee;
-	euler[timee]=node;
-	++timee;
-	for(int x:edge[node]){
-		if(x!=par)
-			dfs(x,edge,node);
+	ett(T n,vector<vector<T>> e,T start){ // no of nodes , edge, starting node of tree
+		in=vector<T>(n+1);
+		out=vector<T>(n+1);
+		euler=vector<T>(2*(n+1));
+		dfs(start,e,-1);
+		
 	}
-	out[node]=timee;
-	euler[timee]=node;
-	++timee;
+
+	void dfs(int node,vector<vector<int>> edge,int par){
+
+		in[node]=timee;
+		euler[timee]=node;
+		++timee;
+		for(int x:edge[node]){
+			if(x!=par)
+				dfs(x,edge,node);
+		}
+		out[node]=timee;
+		euler[timee]=node;
+		++timee;
+	}
 }
