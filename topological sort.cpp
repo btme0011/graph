1de2f958a641e81fd8ee762d40d27f116ec5d2d2
btme0011 <71667635+btme0011@users.noreply.github.com>
2021-04-21 11:58:18 +0530

    Update topological sort.cpp

--- topological sort.cpp
+++ topological sort.cpp
@@ -1,36 +1,38 @@
-bool visited[10];
-void dfs(ll node,vector<vector<ll>> edge,vector<bool> vis,stack<ll> &ans){
-	visited[node]=1;
-	for(auto x:edge[node]){
-		if(visited[x]==0) dfs(x,edge,vis,ans);
-	}
-	ans.push(node);
-}
-string solve(){
-	int n,m;
-	cin>>n>>m;
-	
-	vector<vector<ll>> edge(n);
-	for(int i=0;i<m;i++){
-		int a,b;
-		cin>>a>>b;
-		
-		edge[a].push_back(b);
+template<typename T>
+struct ts{
+	T N;
+	vector<bool> visited;
+	vector<ll> last;// final ans;
+	ts(T n,vector<vecto<T>> edge){// no of nodes, edge
+		N=n;
+		visited=vector<bool>(n+1);
+		solve(n,edge);
 	}
 	
-	fo(i,n){
-		visited[i]=0;
-	}
 	
-	vector<bool> vis(10);
-	stack<ll> ans;
-	vector<ll> mans;
-	for(int i=0;i<n;i++){
-		if(visited[i]==0){
-			dfs(i,edge,vis,ans);
-			
+	void dfs(ll node,vector<vector<ll>> edge,vector<bool> vis,stack<ll> &ans){
+		visited[node]=1;
+		for(auto x:edge[node]){
+			if(visited[x]==0) dfs(x,edge,vis,ans);
 		}
+		ans.push(node);
 	}
-	while(!ans.empty()) {mans.push_back(ans.top());ans.pop();}
-	retv(mans);
-}
+	void solve(T n,vector<vector<T>> edge){
+
+
+		fo(i,n){
+			visited[i]=0;
+		}
+
+		vector<bool> vis(n+1);
+		stack<ll> ans;
+		
+		for(int i=0;i<n;i++){
+			if(visited[i]==0){
+				dfs(i,edge,vis,ans);
+			}
+		}
+		while(!ans.empty()) {last.push_back(ans.top());ans.pop();}
+		
+	}
+};
