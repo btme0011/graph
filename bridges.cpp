6d9a4b79eec621ec109e92d0ec90db031adea48b
btme0011 <71667635+btme0011@users.noreply.github.com>
2021-04-26 12:23:14 +0530

    Update bridges.cpp

--- bridges.cpp
+++ bridges.cpp
@@ -3,24 +3,24 @@ struct bridge{
     vector<vector<T>> edge;
     vector<bool> vis;
     vector<T> tin;
-    vector<T> tout;
+    vector<T> low;
     T timer=0;
     T n;
-    vector<pair<T,T>> ans;
+    vector<pair<T,T>> ans;// get ans
     bridge(T node,vector<vector<T>> e,T start){// no of nodes , edge , startting node
         edge=e;
         n=node;
         vis=vector<bool>(n);
         tin=vector<T>(n);
-        tout=vector<T>(n);
+        low=vector<T>(n);
         dfs(start,-1);
     }
     void dfs(int v, int p) {
-        visited[v] = true;
+        vis[v] = true;
         tin[v] = low[v] = timer++;
-        for (int to : adj[v]) {
+        for (int to : edge[v]) {
             if (to == p) continue;
-            if (visited[to]) {
+            if (vis[to]) {
                 low[v] = min(low[v], tin[to]);
             } else {
                 dfs(to, v);
